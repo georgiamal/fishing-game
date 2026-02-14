a5153d1351aeb25737e24dc6effcb8125088c217
georgiamal <144332277+georgiamal@users.noreply.github.com>
2026-02-14 19:38:15 +0000

    base fishing

---
+++ game/scenes/fish/fish_data.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var fish_types = {
+	"bass": {"rarity": 1, "value": 10},
+	"trout": {"rarity": 2, "value": 25},
+	"salmon": {"rarity": 3, "value": 50}
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- game/scripts/player.gd
+++ game/scripts/player.gd
@@ -5,11 +5,16 @@ var target_position = Vector2.ZERO
 var is_moving = false
 var last_direction = "down"
 var stop_distance = 5.0
+var water_area
+var is_fishing = false
+var player_near_water = false
 
 @onready var sprite = $AnimatedSprite2D
+@onready var fish_data = preload("res://scenes/fish/fish_data.gd").new()
 
 func _ready() -> void:
 	target_position = global_position
+	water_area = get_node("../map/water")
 	
 func _input(event):
 #	for touch
@@ -20,6 +25,8 @@ func _input(event):
 	elif event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		target_position = get_global_mouse_position()
 		is_moving = true
+	elif event is InputEventKey and event.pressed and event.keycode == KEY_F:
+		try_fishing()
 
 func _physics_process(delta: float) -> void:
 	if is_moving:
@@ -30,14 +37,20 @@ func _physics_process(delta: float) -> void:
 			is_moving = false
 			velocity = Vector2.ZERO
 #			keep direction when idle after movement
-			var idle_direction = "idle_" + last_direction
-			sprite.play(idle_direction)
+			play_animation_for_direction(last_direction, "idle")
 		else:
 			var angle = rad_to_deg(direction.angle())
 			velocity = direction * SPEED
 			update_animation(angle)
-	
+		
+		var previous_position = global_position
 		move_and_slide()
+		
+		if global_position.distance_to(previous_position) < 0.1:
+#			player is stuck
+			is_moving = false
+			velocity = Vector2.ZERO
+			play_animation_for_direction(last_direction, "idle")
 
 func update_animation(angle: float):
 	var result = get_animation_from_angle(angle)
@@ -45,9 +58,8 @@ func update_animation(angle: float):
 		last_direction = result[0]
 		var flip_flag = result[1]
 		
-		var animation_name = "walk_" + last_direction
 		sprite.flip_h = flip_flag
-		sprite.play(animation_name)
+		play_animation_for_direction(last_direction, "walk")
 	
 func get_animation_from_angle(angle):
 #	uses angle and return animation name and flip flag
@@ -69,3 +81,54 @@ func get_animation_from_angle(angle):
 	elif (angle > -67.5 && angle < -22.5):
 		return ["dup", false]
 		
+
+func play_animation_for_direction(direction, type):
+	var animation = type + "_" + direction
+	sprite.play(animation)
+
+func try_fishing():	
+	
+	if player_near_water:
+		#print("fishing")
+		start_fishing()
+	else:
+		print("too far")
+
+
+func _on_water_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		player_near_water = true
+		print("in range")
+
+
+func _on_water_body_exited(body: Node2D) -> void:
+	if body.name == "player":
+		player_near_water = false
+		print("out of range")
+
+func start_fishing():
+	is_fishing = true
+	is_moving = false
+	velocity = Vector2.ZERO
+#	TODO: add fishing animation here
+
+	await get_tree().create_timer(2.0).timeout
+	
+	catch_fish()
+
+func catch_fish():
+	var fish_names = fish_data.fish_types.keys()
+	
+	# Pick a random one
+	var random_fish = fish_names[randi() % fish_names.size()]
+
+	# Get that fish's info
+	var fish_info = fish_data.fish_types[random_fish]
+	
+	print("Caught a " + random_fish + "!")
+	print("Rarity: " + str(fish_info["rarity"]))
+	print("Value: " + str(fish_info["value"]))
+	
+	# TODO: Add to inventory
+	is_fishing = false
+	play_animation_for_direction(last_direction, "idle")
